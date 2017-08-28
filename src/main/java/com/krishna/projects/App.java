38e52820190e74a0d2e545e48260f5c3491a06d3
Guduri Sriram <guduri.sriram@inmobi.com>
2017-08-29 00:23:58 +0530

    challenge completed

--- src/main/java/com/krishna/projects/App.java
+++ src/main/java/com/krishna/projects/App.java
@@ -4,29 +4,42 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
         try {
-            createEmployeeTable();
+            createAndPopulateEmployeeTable();
         } catch ( Exception e ) {
             System.out.println("Table exists");
         } finally {
-            addEmployees();
-            printEmployees();
+            Scanner reader = new Scanner(System.in);  // Reading from System.in
+            System.out.print("Enter a employee id or exit to quit: ");
+            String n = reader.next();
+            int id;
+            while (!(n.equalsIgnoreCase("exit") || n.equalsIgnoreCase("quit"))) {
+                try {
+                    id = Integer.parseInt(n);
+                    printEmployeeById(id);
+                    System.out.print("Enter a employee id or exit to quit: ");
+                } catch (Exception e) {
+                    System.out.print("Enter valid integer or Exit to quit :");
+                }
+                n = reader.next();
+            }
         }
     }
 
-    static void createEmployeeTable() {
+    static void createAndPopulateEmployeeTable() {
         Connection c = null;
         Statement stmt = null;
 
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:company.db");
-            System.out.println("Opened database successfully");
+            c.setAutoCommit(false);
 
             stmt = c.createStatement();
             String sql = "CREATE TABLE EMPLOYEE " +
@@ -35,26 +48,8 @@ public class App
                     " AGE            INT     NOT NULL, " +
                     " SALARY         REAL)";
             stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-            System.out.println("Table created successfully");
-        } catch ( Exception e ) {
-            System.out.println("EMPLOYEE TABLE EXISTS");
-        }
-    }
 
-    static void addEmployees() {
-        Connection c = null;
-        Statement stmt = null;
-
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:company.db");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
-
-            stmt = c.createStatement();
-            String sql = "INSERT INTO EMPLOYEE (ID,NAME,AGE,SALARY) " +
+            sql = "INSERT INTO EMPLOYEE (ID,NAME,AGE,SALARY) " +
                     "VALUES (1, 'Paul', 32, 20000.00 );";
             stmt.executeUpdate(sql);
 
@@ -70,13 +65,17 @@ public class App
                     "VALUES (4, 'Mark', 25, 65000.00 );";
             stmt.executeUpdate(sql);
 
+            sql = "INSERT INTO EMPLOYEE (ID,NAME,AGE,SALARY) " +
+                    "VALUES (5, 'Mark', 25, 65000.00 );";
+            stmt.executeUpdate(sql);
+
             stmt.close();
             c.commit();
             c.close();
+            System.out.println("Table created successfully");
         } catch ( Exception e ) {
-
+            System.out.println("EMPLOYEE table already exists");
         }
-        System.out.println("Records created successfully");
     }
 
     static void printEmployees() {
@@ -87,17 +86,49 @@ public class App
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:company.db");
             c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
-            String sql = "DELETE from EMPLOYEE where ID=2;";
-            stmt.executeUpdate(sql);
-            c.commit();
 
             ResultSet rs = stmt.executeQuery( "SELECT * FROM EMPLOYEE;" );
 
             while ( rs.next() ) {
                 int id = rs.getInt("id");
+                String  name = rs.getString("name");
+                int age  = rs.getInt("age");
+                float salary = rs.getFloat("salary");
+
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "AGE = " + age );
+                System.out.println( "SALARY = " + salary );
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
+
+    static void printEmployeeById(int id) {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:company.db");
+            c.setAutoCommit(false);
+            System.out.println();
+            stmt = c.createStatement();
+            String sql = "DELETE from EMPLOYEE where ID=2;";
+            stmt.executeUpdate(sql);
+            c.commit();
+
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM EMPLOYEE where id ="+id+";" );
+
+            while ( rs.next() ) {
                 String  name = rs.getString("name");
                 int age  = rs.getInt("age");
                 float salary = rs.getFloat("salary");
